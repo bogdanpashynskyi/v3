bbdbb7e775b6b9069ea439bcc577038cb5ab97ce
Bogdan Pashynskyi <bogdanpashynskyi@Bogdans-MacBook-Pro.local>
2019-05-12 23:27:14 +0300

    Updates

---
+++ precache-manifest.e0acc3050e8395ad1927fc24e6cce1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d968df6528ac5b8e82d8f3c36443e43",
+    "url": "/v3/index.html"
+  },
+  {
+    "revision": "2785f35f5c4df3d2dabc",
+    "url": "/v3/static/css/main.a4da7bff.chunk.css"
+  },
+  {
+    "revision": "143e2add9a17b02ebe08",
+    "url": "/v3/static/js/2.ebe5abef.chunk.js"
+  },
+  {
+    "revision": "2785f35f5c4df3d2dabc",
+    "url": "/v3/static/js/main.8309dff3.chunk.js"
+  },
+  {
+    "revision": "2233f0a171fd260d702f",
+    "url": "/v3/static/js/runtime~main.88f5b7f6.js"
+  },
+  {
+    "revision": "ed80c0a7985eec3b1e6fe46437c2ffb1",
+    "url": "/v3/static/media/logo.ed80c0a7.png"
+  }
+]);
